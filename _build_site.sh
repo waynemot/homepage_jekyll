2204185f5ab9ec372e0ac529f238637c27edd726
Wayne Motycka <wmotycka@cse.unl.edu>
2021-12-31 16:29:18 -0600

    make _build_site.sh rebuild only on new file chgs

--- _build_site.sh
+++ _build_site.sh
@@ -4,7 +4,19 @@ PATH=$PATH:${HOME}/bin
 
 if [[ `uname -n` -eq "csce" ]]
 then
-    jekyll build && cp -r _site/* ${HOME}/public_html/.
+    rebuild=0
+    for f in  `find . -type f -not -path "*/*.sh" -not -path "./.git/*" -not -path "./.DS_Store" -not -path "./.jekyll-cache/*" -not -path "./_site/*" -not -path "./.idea/*" -not -path "./.git-ignore"`
+    do
+        if [ $f -nt ../index.html ]
+        then
+          rebuild=1
+          break
+        fi
+    done
+    if [[ $rebuild = 1 ]]
+    then
+        jekyll build && cp -r _site/* ${HOME}/public_html/.
+    fi
 else
     echo "this script only executes on the CSCE system"
     exit 1
