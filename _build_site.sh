d359916295a545be52eff2afd0dae831bc161039
Wayne Motycka <wmotycka@cse.unl.edu>
2021-12-31 15:42:29 -0600

    make build script sense uname node-name == csce

--- _build_site.sh
+++ _build_site.sh
@@ -2,5 +2,10 @@
 
 PATH=$PATH:${HOME}/bin
 
-jekyll build && cp -r _site/* ${HOME}/public_html/.
-
+if [[ `uname -n` -eq "csce" ]]
+then
+    jekyll build && cp -r _site/* ${HOME}/public_html/.
+else
+    echo "this script only executes on the CSCE system"
+    exit 1
+fi
